62080bcbe90a1084ee7f9d7c48d7fd93239c509a
Stonetenth2005 <srivastavatushar2005@gmail.com>
2025-07-19 23:13:53 +0530

    choosing color and creatinng cards

--- script.js
+++ script.js
@@ -1,1 +1,44 @@
-lucide.createIcons();
+let plus = document.querySelector('.plus');
+let taskArea = document.querySelector('.task-area');
+
+
+let selectedColour;
+
+let color = document.querySelectorAll('.color-option');
+
+color.forEach(function(element){
+    element.addEventListener('click', function(){
+        color.forEach(function(element){
+            element.style.border = '1px solid rgba(255, 255, 255, 0.2)';
+        })
+        selectedColour = this.style.backgroundColor;
+        this.style.border = '3px solid grey'
+    })
+})
+
+plus.addEventListener('click', function(){
+    let newCard = document.createElement('div');
+    newCard.classList.add('task-card');
+    if(selectedColour=='pink'){
+        newCard.style.backgroundColor = 'rgba(255, 182, 193, 0.25)'
+    }
+    else if(selectedColour=='lightgreen'){
+        newCard.style.backgroundColor = 'rgba(144, 238, 144, 0.25)'
+    }
+    else if(selectedColour=='lightblue'){
+        newCard.style.backgroundColor = 'rgba(173, 216, 230, 0.25)'
+    }
+    else if(selectedColour=='gold'){
+        newCard.style.backgroundColor = 'rgba(255, 215, 0, 0.25)'
+    }
+    taskArea.appendChild(newCard);
+})
+
+document.addEventListener('click', function(e){
+    if((!e.target.classList.contains('color-option') && !e.target.classList.contains('plus'))){
+        color.forEach(function(element){
+            element.style.border = '1px solid rgba(255, 255, 255, 0.2)';
+        });
+        selectedColour = '';
+    }
+});
